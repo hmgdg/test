75a22442e3920fd732a4995169038aa1a6ba2222
Administrator <Administrator@RIZAL3HP591EKUN>
2020-03-07 14:19:43 +0800

    hello gits

---
+++ sp06-ribbon/src/main/java/com/tedu/sp06/Sp06RibbonApplication.java
@@ -0,0 +1,23 @@
+package com.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp06RibbonApplication {
+
+	@LoadBalanced 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Sp06RibbonApplication.class, args);
+	}
+
+}
